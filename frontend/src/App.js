2c3a26212bcae6bf273f219623b97f3aab538a12
Borirat Yothisawat <boriratkk@gmail.com>
2026-01-15 01:28:55 +0700

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/orders")
+      .then(res => res.json())
+      .then(setOrders);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Orders</h2>
+      <ul>
+        {orders.map(o => (
+          <li key={o.id}>
+            {o.product} - {o.qty}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
